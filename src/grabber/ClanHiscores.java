752a62e5daae2f133c1daa25f76077456988e4d0
Dylan Weaver <dweaver@umd.edu>
2017-07-07 18:17:59 -0400

    Adds handling of Http response codes, fixes #2

--- src/grabber/ClanHiscores.java
+++ src/grabber/ClanHiscores.java
@@ -2,11 +2,13 @@ package grabber;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import javax.xml.ws.http.HTTPException;
 
 /**
  * A standalone program that ranks a list of players in order by total level
@@ -17,70 +19,85 @@ import java.util.List;
  */
 public class ClanHiscores {
 	// An arraylist of player objects which will later be sorted and printed
-	private static List<Player> players = new ArrayList<Player>();
+	private static List<Player> players = new ArrayList<>();
 	// Marks the position/rank (NOT index) that we are at in a list
 	private static int position = 1;
 	// The current line of data - null before any data is read
 	private static String currentLine = null;
+	// Any excluded players (due to errors) will br place in here
+	private static List<String> playerErrors = new ArrayList<>();
 
 	/**
 	 * For the given position, connect to the hiscores page for the username and
 	 * read the data
 	 * 
-	 * @param position The current index of the array
+	 * @param position
+	 *            The current index of the array
 	 */
-	private static void connect(int position) {
-		// Connect to the hiscores using the username found at the position index of Utlity.playersList
+	private static boolean connect(int position) {
+		// Connect to the hiscores using the username found at the position index of
+		// Utlity.playersList
 		String stringURL = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player="
 				+ Utility.playersList[position].trim().replaceAll(" ", "_");
 		try {
 			// Open a connection to StringUrl
 			URL url = new URL(stringURL);
-			URLConnection conn = url.openConnection();
-			conn.connect();
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			if (conn.getResponseCode() == 404) {
+				throw new HTTPException(404);
+			}
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			// Read the line of data (which contains overall level, rank, and experience)
 			currentLine = in.readLine();
 			// Close the stream
 			in.close();
+			return true;
+		} catch (HTTPException e) {
+			if (e.getStatusCode() == 404) {
+				playerErrors.add(Utility.playersList[position]);
+				System.out.println(
+						"404 returned for: " + "\"" + Utility.playersList[position].replaceAll(" ", "_") + "\"");
+			}
+			return false;
 		} catch (Exception e) {
 			System.out.println("Error connecting " + e);
+			return false;
 		}
 	}
-	
+
 	/**
-	 * Grabs the level and experience data for each player, adding it to a player object
-	 * and then adding the player object into an arraylist
+	 * Grabs the level and experience data for each player, adding it to a player
+	 * object and then adding the player object into an arraylist
 	 */
 	private static void runGrabber() {
 		System.out.println("CLAN HISCORES");
 		System.out.println("----");
-		System.out.println(
-				"Please be patient, this takes a bit. Any errors (such as a name change or incorrect username)"
-				+ " will appear directly below this line.");
-			// Go through the entire list of players
-			for (int i = 0; i < Utility.playersList.length; i++) {
-				try {
-					// Connect to the hiscores for the current index
-					connect(i);
-					
-					// Break up the line into an array of rank, level, and experience respectively 
+		System.out
+				.println("Please be patient, this takes a bit. Any errors (such as a name change or incorrect username)"
+						+ " will appear directly below this line.");
+		// Go through the entire list of players
+		for (int i = 0; i < Utility.playersList.length; i++) {
+			try {
+				// Connect to the hiscores for the current index
+				if (connect(i)) {
+					// Break up the line into an array of rank, level, and experience respectively
 					String[] skillBrokenUp = currentLine.replaceAll(" ", "").split(",");
 					int level = Integer.parseInt(skillBrokenUp[1]);
 					int experience = Integer.parseInt(skillBrokenUp[2]);
-					
+
 					// Create a Player object and add this object to the players arrayList
 					Player p = new Player(Utility.playersList[i], level, experience);
 					players.add(p);
-				} catch (Exception e) {
-					// Useful in cases of a misspelled name or a name change within the clan
-					e.printStackTrace();
 				}
+			} catch (Exception e) {
+				// Useful in cases of a misspelled name or a name change within the clan
+				e.printStackTrace();
 			}
-			// Sort the list by level and experience using Player's compareTo method
-			Collections.sort(players, Collections.reverseOrder());
-			// We exited the loop, so we're done!
-			System.out.println("\nDONE\n");
+		}
+		// Sort the list by level and experience using Player's compareTo method
+		Collections.sort(players, Collections.reverseOrder());
+		// We exited the loop, so we're done!
+		System.out.println("\nDONE\n");
 	}
 
 	/**
@@ -89,6 +106,17 @@ public class ClanHiscores {
 	private static void printHiscores() {
 		runGrabber();
 
+		// Print a list of all the players that aren't included so the user is aware
+		// that the results may be incorrect
+		if (!playerErrors.isEmpty()) {
+			System.out.println("Players excluded from results due to errors:");
+			for (String players : playerErrors) {
+				System.out.println(players.replace(" ", "_"));
+			}
+		}
+
+		// Print the final rankings
+		System.out.println("\nResults:");
 		for (Player list : players) {
 			System.out.println(position + Utility.getPositionSuffix(position) + list.toString());
 			position++;
--- src/grabber/Grabber.java
+++ src/grabber/Grabber.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.ws.http.HTTPException;
+
 /**
  * Grabs data about a user specified skill for a list of players
  * 
@@ -17,25 +19,31 @@ import java.util.List;
 public class Grabber extends Thread {
 	private static int counter; // This is used to make the output a little bit nicer
 	private static String currentLine = null; // The current line of data - null before anything is read
-	private static List<Player> players = new ArrayList<Player>(); // Stores name, experience, and level data for each player
-	int threads = Runtime.getRuntime().availableProcessors(); //The number of threads to create
+	private static List<Player> players = new ArrayList<Player>(); // Stores name, experience, level data for each player
+	int threads = Runtime.getRuntime().availableProcessors(); // The number of threads to create
+	private static List<String> playerErrors = new ArrayList<>(); // Any excluded players (due to errors) will be here
 
 	/**
 	 * For the given position, connect to the hiscores page for the username and
 	 * read the data
 	 * 
-	 * @param skill The skill that we are collecting data for
-	 * @param position The current index of the array
+	 * @param skill
+	 *            The skill that we are collecting data for
+	 * @param position
+	 *            The current index of the array
 	 */
-	private static void connect(String skill, int position) {
-		// Connect to the hiscores using the username found at the position index of Utlity.playersList
+	private static boolean connect(String skill, int position) {
+		// Connect to the hiscores using the username found at the position index of
+		// Utlity.playersList
 		String stringURL = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player="
 				+ Utility.playersList[position].replaceAll(" ", "_");
 		try {
 			// Open a connection to StringUrl
 			URL url = new URL(stringURL);
-			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-			conn.connect();
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			if (conn.getResponseCode() == 404) {
+				throw new HTTPException(404);
+			}
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			// Go to the correct line where the skill information is
 			for (int i = 0; i < Arrays.asList(Utility.SKILL_NAMES).indexOf(skill); i++) {
@@ -45,36 +53,51 @@ public class Grabber extends Thread {
 			currentLine = in.readLine();
 			// Close the stream
 			in.close();
+			return true;
+		} catch (HTTPException e) {
+			if (e.getStatusCode() == 404) {
+				playerErrors.add(Utility.playersList[position]);
+				System.out.println("404 returned for: " + "\"" + Utility.playersList[position] + "\""
+						+ ". Fix this before using the printed data.");
+			}
+			return false;
 		} catch (Exception e) {
-			// Occurs when a connection can't be established TODO handle 404
+			// Occurs when a connection can't be established
 			System.out.println("Error connecting " + e);
+			return false;
 		}
 	}
 
 	/**
-	 * Connects to the OSRS hiscores for each player, grabbing data for each
-	 * member in the <CODE>Utility.playersList</CODE> array, and storing that in <CODE>players</CODE>
+	 * Connects to the OSRS hiscores for each player, grabbing data for each member
+	 * in the <CODE>Utility.playersList</CODE> array, and storing that in
+	 * <CODE>players</CODE>
 	 * 
-	 * @param skill The skill to collect data for
+	 * @param skill
+	 *            The skill to collect data for
 	 */
 	private static void runGrabber(String skill) {
 		// Only continue if skill exists in the Utility.SKILL_NAMES array
 		if (checkForValidSkill(skill)) {
 			System.out.println("DATA FOR " + skill.toUpperCase() + " EVENT");
-			System.out.println("Please wait while the data processes...");
+			System.out.println("Please wait while the data processes... Errors will appear directly below");
 			for (int i = 0; i < Utility.playersList.length; i++) {
 				try {
-					// Connect to the hiscores with a reference of the current position so we know which player to process
-					connect(skill, i);
-					
-					// Break up the current line into an array which consists of rank, level, experience respectively
-					String[] skillBrokenUp = currentLine.replaceAll(" ", "").split(",");
-					int level = Integer.parseInt(skillBrokenUp[1]);
-					int experience = Integer.parseInt(skillBrokenUp[2]) == -1 ? 0 :  Integer.parseInt(skillBrokenUp[2]);
-					
-					// Create a new Player object which stores the current name, level and experience and add that to Players
-					Player p = new Player(Utility.playersList[i], level, experience);
-					players.add(p);
+					// Connect to the hiscores with a reference of the current position so we know
+					// which player to process
+					if (connect(skill, i)) {
+						// Break up the current line into an array which consists of rank, level,
+						// experience respectively
+						String[] skillBrokenUp = currentLine.replaceAll(" ", "").split(",");
+						int level = Integer.parseInt(skillBrokenUp[1]);
+						int experience = Integer.parseInt(skillBrokenUp[2]) == -1 ? 0
+								: Integer.parseInt(skillBrokenUp[2]);
+
+						// Create a new Player object which stores the current name, level and
+						// experience and add that to Players
+						Player p = new Player(Utility.playersList[i], level, experience);
+						players.add(p);
+					}
 				} catch (Exception e) {
 					// Useful in cases of a misspelled name or a name change within the clan
 					System.out.println(Utility.capitalizeFirst(Utility.playersList[i].trim() + "'s "
@@ -85,7 +108,7 @@ public class Grabber extends Thread {
 			System.out.println("Error, invalid skill name.");
 		}
 	}
-	
+
 	/**
 	 * Prints out the experience data for each player
 	 */
@@ -101,7 +124,7 @@ public class Grabber extends Thread {
 		// Reset the counter
 		counter = 0;
 	}
-	
+
 	/**
 	 * Prints out the level data for each player
 	 */
@@ -117,11 +140,12 @@ public class Grabber extends Thread {
 		// Reset the counter
 		counter = 0;
 	}
-	
+
 	/**
-	 * Checks that <CODE>skill</CODE> is in <CODE>Utility.SKILL_NAMES</CODE> 
+	 * Checks that <CODE>skill</CODE> is in <CODE>Utility.SKILL_NAMES</CODE>
 	 * 
-	 * @param str The string to check against SKILL_NAMES
+	 * @param str
+	 *            The string to check against SKILL_NAMES
 	 */
 	static boolean checkForValidSkill(String str) {
 		for (int i = 0; i < Utility.SKILL_NAMES.length; i++) {
@@ -134,10 +158,19 @@ public class Grabber extends Thread {
 
 	public static void main(String[] args) {
 		runGrabber("hunter");
-		
+
+		// Print a list of all the players that aren't included so the user is aware
+		// that the results may be incorrect
+		if (!playerErrors.isEmpty()) {
+			System.out.println("\nPlayers excluded from results due to errors:");
+			for (String players : playerErrors) {
+				System.out.println(players.replace(" ", "_"));
+			}
+		}
+
 		System.out.println("\n--- Experience Data ---");
 		printExperience();
-		
+
 		System.out.println("\n\n--- Level Data ---");
 		printLevel();
 	}
